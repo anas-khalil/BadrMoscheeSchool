a858ee4ea7057122d02af62b7f565ee7bd8565d9
Anas Saleh <anas.khalil.mohammed@gmail.com>
2026-09-25 19:41:16 +0200

    fix: start messaging directory sync at app startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { startDirectorySync } from './directorySync';
 import './styles.css';
 import './styles-mobile.css';
+
+startDirectorySync();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
